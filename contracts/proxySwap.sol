d571373b44687510f844e3e9a2b6b8649e087095
Quincy Jones <rqjones@asu.edu>
2022-11-28 19:29:57 -0700

    hardhat

---
+++ contracts/proxySwap.sol
@@ -0,0 +1,58 @@
+pragma solidity ^0.8.10;
+// SPDX-License-Identifier: MIT 
+import "./RouterV2.flat.sol"; //Globiance
+
+//aggregated swap that makes descions to maximize liquidity among dexes
+contract DAppProxySwap{
+
+    constructor(){}
+
+    function SwapIn(address _token)public returns(bool){}
+
+    function SwapOut(address _token, uint _total)public returns(bool){}
+}
+
+//DEXs
+contract GlobianceProxySwap{
+
+    constructor(){}
+
+    function SwapIn(address _token)public returns(bool){}
+
+    function SwapOut(address _token, uint _total)public returns(bool){}
+}
+
+contract XSWAPProxySwap{
+
+    constructor(){}
+
+    function SwapIn(address _token)public returns(bool){}
+
+    function SwapOut(address _token, uint _total)public returns(bool){}
+}
+
+//Bridges
+contract SWIFTAllChainBridgeProxySwap{
+
+    constructor(){}
+
+    function SwapIn(address _token)public returns(bool){}
+
+    function SwapOut(address _token, uint _total)public returns(bool){}
+}
+contract ImpelProxySwap{
+
+    constructor(){}
+
+    function SwapIn(address _token)public returns(bool){}
+
+    function SwapOut(address _token, uint _total)public returns(bool){}
+}
+contract WanChainProxySwap{
+
+    constructor(){}
+
+    function SwapIn(address _token)public returns(bool){}
+
+    function SwapOut(address _token, uint _total)public returns(bool){}
+}
